608d339c62f1805288128e9167d0dc39b71e15a6
Andy <andydavies2020@gmail.com>
2012-10-26 09:22:57 +0100

    Initial import

---
+++ DarkChatLinks.lua
@@ -0,0 +1,73 @@
+
+local URL_COLOR = "16FF5D"
+
+local originalSetItemRef = SetItemRef
+local replacementSetItemRef = function(link, text, button, chatFrame)
+
+	if strsub(link, 1, 3) == "url" then
+
+		local chatFrameEditBox = ChatEdit_ChooseBoxForSend()
+		local url = strsub(link, 5)
+
+		if not chatFrameEditBox:IsShown() then
+			ChatEdit_ActivateChat(chatFrameEditBox)
+		end
+
+		chatFrameEditBox:Insert(url)
+		chatFrameEditBox:HighlightText()
+
+	else
+		originalSetItemRef(link, text, button, chatFrame)
+	end
+	
+end
+SetItemRef = replacementSetItemRef
+
+
+local createUrl = function(url)
+	return " |cff".. URL_COLOR .. "|Hurl:" .. url .. "|h" .. url .. "|h|r "
+end
+
+local addLinkSyntax = function(chatstring)
+
+	if type(chatstring) == "string" then
+	
+		local extraspace
+		
+		if not strfind(chatstring, "^ ") then
+			extraspace = true
+			chatstring = " " .. chatstring
+		end
+		
+		chatstring = gsub (chatstring, " www%.([_A-Za-z0-9-]+)%.(%S+)%s?", createUrl("www.%1.%2"))
+		chatstring = gsub (chatstring, " (%a+)://(%S+)%s?", createUrl("%1://%2"))
+		chatstring = gsub (chatstring, " ([_A-Za-z0-9-%.]+)@([_A-Za-z0-9-]+)(%.+)([_A-Za-z0-9-%.]+)%s?", createUrl("%1@%2%3%4"))
+		chatstring = gsub (chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?):(%d%d?%d?%d?%d?)%s?", createUrl("%1.%2.%3.%4:%5"))
+		chatstring = gsub (chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%s?", createUrl("%1.%2.%3.%4"))
+		
+		if extraspace then
+			chatstring = strsub(chatstring, 2)
+		end
+		
+	end
+	
+	return chatstring
+end
+
+local initialise = function()
+	
+	for i = 1, NUM_CHAT_WINDOWS do
+
+		local frame = _G["ChatFrame" .. i]
+
+		local addmessage = frame.AddMessage
+		local replacement = function(self, text, ...) 
+			addmessage(self, addLinkSyntax(text), ...) 
+		end
+		frame.AddMessage = replacement
+
+	end
+
+end
+
+initialise()
